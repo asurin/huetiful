5b216645a205000ec610a784d08ff729b05ea230
asurin <alastairjsurin@gmail.com>
2013-01-19 13:32:31 -0500

    Skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Huetiful::Application.config.secret_token = '0ad722f767302853664b9f358859639dbe58f87e3a3e6019c0f735dd0c1bcededa873c0012197c80e38697e6c17e17399f7370bb9e0dda43f816ac166b706cea'
